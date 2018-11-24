715715ad60f001fb8cc0d0b69e8d95ad8354fc3f
Lyndon Bibera <lyndon.bibera@boltoninternational.com>
2018-11-24 22:05:23 +0800

    generated a spring project from start.spring.io

---
+++ shopping-cart-backend-java/src/main/java/com/drakeintl/shoppingcart/backend/ShoppingCartBackendJavaApplication.java
@@ -0,0 +1,12 @@
+package com.drakeintl.shoppingcart.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartBackendJavaApplication.class, args);
+	}
+}
