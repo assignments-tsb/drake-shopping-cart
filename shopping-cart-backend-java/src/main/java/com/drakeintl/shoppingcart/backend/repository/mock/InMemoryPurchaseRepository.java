ec32b3c2b545588c2abf86fa6955d82b2887474e
Lyndon Bibera <lyndon.bibera@boltoninternational.com>
2018-11-26 10:59:26 +0800

    added concrete implementation for the purchase repo

--- shopping-cart-backend-java/src/main/java/com/drakeintl/shoppingcart/backend/repository/mock/InMemoryPurchaseRepository.java
+++ shopping-cart-backend-java/src/main/java/com/drakeintl/shoppingcart/backend/repository/mock/InMemoryPurchaseRepository.java
@@ -2,31 +2,54 @@ package com.drakeintl.shoppingcart.backend.repository.mock;
 
 import com.drakeintl.shoppingcart.backend.model.Purchase;
 import com.drakeintl.shoppingcart.backend.repository.PurchaseRepository;
+import com.google.common.collect.Lists;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
+// in a real-world application, we should not be
+// writing this duplicate code; spring data
+// should weave the implementation at runtime
+// we only have to declare the interfaces
 @Component
 public class InMemoryPurchaseRepository implements PurchaseRepository {
 
+    private final AtomicLong serialId = new AtomicLong();
+    private final Map<Long, Purchase> storage = new HashMap<>();
+
     @Override
-    public Optional<Purchase> find(Long aLong) {
-        return Optional.empty();
+    public Optional<Purchase> find(Long id) {
+        return Optional.ofNullable(storage.get(id));
     }
 
     @Override
-    public List<Purchase> find(Iterable<Long> longs) {
-        return null;
+    public List<Purchase> find(Iterable<Long> ids) {
+        synchronized (storage) {
+            return StreamSupport.stream(ids.spliterator(), false)
+                    .flatMap( (id) -> Stream.of(storage.get(id)))
+                    .collect(Collectors.toList());
+        }
     }
 
     @Override
     public List<Purchase> findAll() {
-        return null;
+        return Collections.unmodifiableList(new ArrayList<>(storage.values()));
     }
 
     @Override
-    public List<Purchase> save(Iterable<Purchase> entities) {
-        return null;
+    public List<Purchase> save(Iterable<Purchase> purchases) {
+        synchronized (storage) {
+            for (Purchase purchase : purchases) {
+                if (purchase.getId() == null) {
+                    purchase.setId(serialId.incrementAndGet());
+                }
+                storage.put(purchase.getId(), purchase);
+            }
+        }
+        return Collections.unmodifiableList(Lists.newArrayList(purchases));
     }
 }
